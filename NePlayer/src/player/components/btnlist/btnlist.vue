447f3f9d97d1d10cdb676b9a66739b8209b9d75a
nebulas <1198398580@qq.com>
2018-05-24 16:18:46 +0800

    2018/5/24 start project

---
+++ NePlayer/src/player/components/btnlist/btnlist.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="btnlist">
+      <div class="play-mode">
+         
+      </div>
+      <div class="play-control">
+         <font-awesome-icon :icon="['fas','fa-play-circle']"></font-awesome-icon>
+      </div>
+      <div class="play-volume">
+          
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus" scoped>
+    #btnlist{
+        position absolute
+        bottom 150px
+        height 100px
+        width 400px
+    }
+    .btn-icon{
+        display block
+        cursor pointer   
+    }
+    .music-prev{
+        width 19px
+        height 20px   
+    }
+</style>
---
+++ NePlayer/src/player/player.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="player">
+    <Btnlist></Btnlist>
+  </div>
+</template>
+<script>
+import Btnlist from "./components/btnlist/btnlist"
+export default {
+  components:{
+    Btnlist
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
